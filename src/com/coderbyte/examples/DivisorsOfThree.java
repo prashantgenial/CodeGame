8196a212d81d96e805738adc652a2404b7af6d28
Prashant G <prashant.gupta.ext@orange.com>
2018-07-20 11:05:32 +0530

    Added Divisor of three

---
+++ src/com/coderbyte/examples/DivisorsOfThree.java
@@ -0,0 +1,36 @@
+package com.coderbyte.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/*
+ * You will be given 2 parameters: a low and high number. Your goal is to print all numbers between low and high,
+ *  and for each of these numbers print whether or not the number is divisible by 3. If the number is divisible by 3, print the word "div3"
+ *  directly after the number.
+ * */
+
+public class DivisorsOfThree {
+
+	public static void main(String[] args) {
+		
+		test_divisior1(2,20);
+
+	}
+
+	public static void test_divisior(int low,int high) {
+		
+		List<Integer> output = new ArrayList<>();
+		
+		for (int i = low; i <= high; i++) {
+		    // check if the current number is evenly divisible by 3
+		    if (i % 3 == 0) {  output.add(i); }		    
+		}
+		
+		output.stream().forEach(System.out::println);
+	}
+	
+	public static void test_divisior1(int low,int high) {
+		IntStream.range(low,high).filter(i -> i%3 == 0).forEach(System.out::println);
+	}
+}
