ac9961bcd06fafa29b27528821298d3b3533454d
Gupta Prashant <prashant.gupta.ext@orange.com>
2019-02-12 11:15:05 +0530

    added Treemap demo in MapDemo class

--- src/com/mine/MapDemo.java
+++ src/com/mine/MapDemo.java
@@ -6,6 +6,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * 
@@ -140,11 +141,32 @@ public class MapDemo {
 		  lruCache.put("e", "E");
 		  System.out.println(lruCache);		  
 	  }
+	  
+  private static void treeMapDemo() {
+		System.out.println("\n\nInside treeMapDemo ...");
+		TreeMap<String, Integer> map1 = new TreeMap<>();
+		map1.put("John", 25);
+		map1.put("Raj", 29);
+		map1.put("Anita", 23);
+		
+		System.out.println(map1);	
+		
+		System.out.println("Iterating using entrySet ...");
+		Set<Map.Entry<String, Integer>> mappings = map1.entrySet();
+		for (Map.Entry<String, Integer> mapping : mappings) {
+			System.out.println("Name: " + mapping.getKey() + ", Age: " + mapping.getValue());
+			if (mapping.getKey().equals("John"))
+				mapping.setValue(26);	
+		}
+		System.out.println(map1);
+		map1.floorEntry("Raj").setValue(22);
+	}  
 		
 	public static void main(String[] args) {
 		// hashMapDemo();	
 		// immutableKeysDemo();		
-		lruCacheTest();				
+		//lruCacheTest();
+		treeMapDemo();
 	}
 	
 }
--- src/com/mine/ReverseStr.java
+++ src/com/mine/ReverseStr.java
@@ -2,11 +2,14 @@ package com.mine;
 
 public class ReverseStr {
 	public static void main(String[] args) {
-		String a = "java" ;
+		//String a = "java" ;
+		String a = "hooh" ;
 		StringBuilder sb = new StringBuilder(a);
 		String r = sb.reverse().toString();
 		if(r.equalsIgnoreCase(a)) {
 			System.out.println("yes");
 		}
+		else
+			System.out.println("NO");
 	}
 }
