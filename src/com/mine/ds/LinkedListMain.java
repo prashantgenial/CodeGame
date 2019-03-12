948aa8d3d881d5770e4ef2d3c8b6396cc79b44a5
Gupta Prashant <prashant.gupta.ext@orange.com>
2019-03-12 17:20:13 +0530

    added algo

---
+++ src/com/mine/ds/LinkedListMain.java
@@ -0,0 +1,19 @@
+package com.mine.ds;
+
+public class LinkedListMain {
+	 
+	 public static void main(String args[])
+	 {
+	   SinglyLinkedList myLinkedlist = new SinglyLinkedList();
+	   myLinkedlist.insertFirst(5);
+	   myLinkedlist.insertFirst(6);
+	   myLinkedlist.insertFirst(7);
+	   myLinkedlist.insertFirst(1);
+	   myLinkedlist.insertLast(2);
+	   myLinkedlist.printLinkedList();
+	   // Linked list will be 
+	   // 2 -> 1 ->  7 -> 6 -> 5
+	   System.out.println("Length of Linked List using iteration: "+myLinkedlist.lengthOfLinkedList());
+	 //  System.out.println("Length of Linked List Using recursion: "+myLinkedlist.lengthOfLinkedListRec(myLinkedlist.getHead()));
+	 }
+	}
